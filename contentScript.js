7885862f8cfc20e6864d542ddcb698bf39801a32
valentin drolet <valentin.drolet@outlook.fr>
2020-04-03 18:41:42 +0200

    Move 'Watch next' and 'Recommanded' collections to the first position

--- contentScript.js
+++ contentScript.js
@@ -21,5 +21,33 @@ function dvWebPlayerMutated() {
   }
 }
 
+function putCollectionsFirst() {
+  var mainDiv = document.getElementById("aiv-cl-main-middle");
+  var firstCollectionDiv = document.getElementsByClassName('_1gQKv6 u-collection tst-collection')[0];
+
+  if (mainDiv && firstCollectionDiv) {
+    mainDiv = mainDiv.firstElementChild;
+    for (var i = collectionsTitlesToPutFirst.length - 1; i >= 0; i--) {
+      const collectionDiv = document.evaluate("//h2[" + collectionsTitlesToPutFirst[i] + "]/ancestor::div[@class='_1gQKv6 u-collection tst-collection']", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+      if (collectionDiv){
+        mainDiv.insertBefore(collectionDiv, firstCollectionDiv);
+        firstCollectionDiv = collectionDiv;
+      }
+    }
+  }
+}
+
+function concatXPathContains(elements) {
+  var concatenated = '';
+  elements.forEach(t => concatenated += (concatenated.length === 0 ? '' : ' or ') + "contains(.,'" + t + "')");
+  return concatenated;
+}
+
 const dvWebPlayerObserver = new MutationObserver(dvWebPlayerMutated);
 dvWebPlayerObserver.observe(document.getElementById('dv-web-player'), { childList: true, subtree: true });
+
+const collectionsTitlesToPutFirst = [
+  concatXPathContains(['Watch next TV and movies', 'Vos programmes - Séries et films']),
+  concatXPathContains(['Recommended movies', 'Films recommandés pour vous'])
+];
+putCollectionsFirst();
